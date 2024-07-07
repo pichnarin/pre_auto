3713f5fd03c25064ae7183187571e2e3eaf1bf90
noobmaster69 <139727372+pichnarin@users.noreply.github.com>
2024-07-08 00:25:28 +0700

    apply function to each controller class

--- crispy_auto/src/main/java/org/nome/pre_auto/DesignFaController.java
+++ crispy_auto/src/main/java/org/nome/pre_auto/DesignFaController.java
@@ -3,18 +3,31 @@ package org.nome.pre_auto;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.HPos;
+import javafx.geometry.VPos;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 public class DesignFaController {
+
+    @FXML
+    private ChoiceBox<String> choiceBox;
 
     @FXML
     private TextField inAlphabet;
@@ -38,7 +51,7 @@ public class DesignFaController {
     private TextField inString;
 
     @FXML
-    private TextArea inTransitions;
+    private TextArea inTransition;
 
     @FXML
     private ImageView outGraph;
@@ -85,7 +98,168 @@ public class DesignFaController {
 
     @FXML
     public void initialize(){
-
+        initializeChoiceBox();
+        initializeSubmitBtn();
+        initializeResetBtn();
     }
 
+    private void initializeSubmitBtn() {
+        //the part of dfa is implemented here
+        submitBtn.setOnAction(_ -> {
+            if (choiceBox.getValue().equals("DFA")){
+
+                outFaType.clear();
+                outFaType.setText("This FA is DFA");
+
+                Set<String> state = new HashSet<>(Arrays.asList(inState.getText().split(",")));
+                Set<String> alphabet = new HashSet<>(Arrays.asList(inAlphabet.getText().split(",")));
+                String startState = inStartState.getText();
+                Set<String> finalState = new HashSet<>(Arrays.asList(inFinalState.getText().split(",")));
+                Set<String> transition = new HashSet<>(Arrays.asList(inTransition.getText().split(",\\s*")));
+                Set<String> initial_string = new HashSet<>(Arrays.asList(inString.getText().split(",")));
+
+                System.out.printf("Alphabet: %s%n", alphabet);
+                System.out.printf("Transition: %s%n", transition);
+
+                // Validate the input
+                if (state.isEmpty() || alphabet.isEmpty() || startState.isEmpty() || finalState.isEmpty() || transition.isEmpty() || initial_string.isEmpty()) {
+                    outResult.setText("Please fill all the fields");
+                    return;
+                } else if (!state.contains(startState)) {
+                    outResult.setText("Start state must be one of the states");
+                    return;
+                } else if (!state.containsAll(finalState)) {
+                    outResult.setText("Final state must be from the states");
+                    return;
+                }
+
+                // You can now use these values in your application
+                PrimaryData primaryData = new PrimaryData(state, alphabet, startState, finalState, transition, initial_string);
+
+                String outputPath = "dfa.png";
+                String dotScript = primaryData.generateDotScript();
+                try {
+                    primaryData.GenerateImage(dotScript, outputPath);
+                    Image image = new Image(new FileInputStream(outputPath));
+                    outGraph.setPreserveRatio(true);
+                    outGraph.setImage(image);
+                    GridPane.setHalignment(outGraph, HPos.CENTER); // Center horizontally in the cell
+                    GridPane.setValignment(outGraph, VPos.CENTER); // Center vertically in the cell
+                } catch (IOException | InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+
+                // Test the input strings
+                String[] testStrings = inString.getText().split(",");
+                outResult.clear(); // Clear previous results
+                int i = 0;
+                for (String testString : testStrings) {
+                    boolean isAccepted = primaryData.isStringAccepted(testString);
+                    outResult.appendText("%d : (%s) = %s\n".formatted(i, testString, isAccepted ? "Accepted" : "Rejected"));
+                    i++;
+                }
+
+            }
+            //if the part of nfa implemented here
+            else if(choiceBox.getValue().equals("NFA")){
+                Set<String> state = new HashSet<>(Arrays.asList(inState.getText().split(",")));
+                Set<String> alphabet = new HashSet<>(Arrays.asList(inAlphabet.getText().split(",")));
+                String startState = inStartState.getText();
+                Set<String> finalState = new HashSet<>(Arrays.asList(inFinalState.getText().split(",")));
+                Set<String> transition = new HashSet<>(Arrays.asList(inTransition.getText().split(",\\s*")));
+                Set<String> initial_string = new HashSet<>(Arrays.asList(inString.getText().split(",")));
+                Set<String> epsilonAlphabet = new HashSet<>(Arrays.asList(inEState.getText().split(",")));
+                Set<String> epsilonTransition = new HashSet<>(Arrays.asList(inETransition.getText().split(",\\s*")));
+
+                //validate the input
+                if (state.isEmpty() || alphabet.isEmpty() || startState.isEmpty() || finalState.isEmpty() || transition.isEmpty() || initial_string.isEmpty() || inEState.getText().isEmpty() || inETransition.getText().isEmpty()) {
+                    outResult.setText("Please fill all the fields");
+                    return;
+                } else if (!state.contains(startState)) {
+                    outResult.setText("Start state must be one of the states");
+                    return;
+                } else if (!state.containsAll(finalState)) {
+                    outResult.setText("Final state must be from the states");
+                    return;
+                }
+
+                outFaType.clear();
+                outFaType.setText("This FA is NFA");
+
+                transition.addAll(epsilonTransition);
+                alphabet.addAll(epsilonAlphabet);
+
+                System.out.printf("Alphabet: %s%n", alphabet);
+                System.out.printf("Transition: %s%n", transition);
+
+
+                PrimaryData secondaryData = new PrimaryData(state, alphabet, startState, finalState, transition, initial_string);
+
+                String outputPath = "nfa.png";
+                String dotScript = secondaryData.generateDotScript();
+                try {
+                    secondaryData.GenerateImage(dotScript, outputPath);
+                    Image image = new Image(new FileInputStream(outputPath));
+                    outGraph.setPreserveRatio(true);
+                    outGraph.setImage(image);
+                    GridPane.setHalignment(outGraph, HPos.CENTER); // Center horizontally in the cell
+                    GridPane.setValignment(outGraph, VPos.CENTER); // Center vertically in the cell
+                } catch (IOException | InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+
+                //test the input strings
+                String[] testStrings = inString.getText().split(",");
+                outResult.clear(); // Clear previous results
+                int i = 0;
+                for (String testString : testStrings) {
+                    boolean isAccepted = secondaryData.isStringAccepted(testString);
+                    outResult.appendText("%d : (%s) = %s\n".formatted(i, testString, isAccepted ? "Accepted" : "Rejected"));
+                    i++;
+                }
+
+            }
+        });
+    }
+
+    private void initializeResetBtn() {
+        resetBtn.setOnAction(_ ->{
+            inAlphabet.clear();
+            inEState.clear();
+            inETransition.clear();
+            inFinalState.clear();
+            inStartState.clear();
+            inState.clear();
+            inString.clear();
+            inTransition.clear();
+            outResult.clear();
+            outFaType.clear();
+            outGraph.setImage(null);
+        });
+    }
+
+    //initialize the choice box for user to pickup dfa or nfa
+    public void initializeChoiceBox(){
+        //initialize the choice box
+        choiceBox.getItems().add("DFA");
+        choiceBox.getItems().add("NFA");
+
+        //set the default value
+        choiceBox.setValue("DFA");
+
+        if (choiceBox.getValue().equals("DFA")) {
+            inEState.setDisable(true);
+            inETransition.setDisable(true);
+        }
+        //the operation of choice box
+        choiceBox.setOnAction(_ -> {
+            if(choiceBox.getValue().equals("DFA")){
+                inEState.setDisable(true);
+                inETransition.setDisable(true);
+            }else if(choiceBox.getValue().equals("NFA")){
+                inEState.setDisable(false);
+                inETransition.setDisable(false);
+            }
+        });
+    }
 }
--- crispy_auto/src/main/java/org/nome/pre_auto/MinimizeDfaController.java
+++ crispy_auto/src/main/java/org/nome/pre_auto/MinimizeDfaController.java
@@ -3,16 +3,25 @@ package org.nome.pre_auto;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.HPos;
+import javafx.geometry.VPos;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 public class MinimizeDfaController {
 
@@ -74,7 +83,80 @@ public class MinimizeDfaController {
 
     @FXML
     public void initialize(){
+        initializeSubmitBtn();
+        initializeResetBtn();
+    }
 
+    private void initializeResetBtn() {
+        resetBtn.setOnAction(_ -> {
+            inState.clear();
+            inAlphabet.clear();
+            inStartState.clear();
+            inFinalState.clear();
+            inTransition.clear();
+            inString.clear();
+            outResult.clear();
+            outGraph.setImage(null);
+        });
+    }
+
+    private void initializeSubmitBtn() {
+        submitBtn.setOnAction(_ -> {
+
+                Set<String> states = new HashSet<>(Arrays.asList(inState.getText().split(",")));
+                Set<String> alphabet = new HashSet<>(Arrays.asList(inAlphabet.getText().split(",")));
+                String startState = inStartState.getText();
+                Set<String> finalStates = new HashSet<>(Arrays.asList(inFinalState.getText().split(",")));
+                Set<String> transitions = new HashSet<>(Arrays.asList(inTransition.getText().split(",\\s*")));
+
+                Set<String> strings = new HashSet<>(Arrays.asList(inString.getText().split(",")));
+                 // Validate the input
+                if (states.isEmpty() || alphabet.isEmpty() || startState.isEmpty() || finalStates.isEmpty() || transitions.isEmpty()) {
+                    outResult.setText("Please fill all the fields");
+                    return;
+                } else if (!states.contains(startState)) {
+                    outResult.setText("Start state must be one of the states");
+                    return;
+                } else if (!states.containsAll(finalStates)) {
+                    outResult.setText("Final state must be from the states");
+                    return;
+                }
+
+                PrimaryData data = new PrimaryData(states, alphabet, startState, finalStates, transitions, strings);
+
+                MinimizeDfa dfa = new MinimizeDfa(states, alphabet, startState, finalStates, transitions);
+                MinimizeDfa minimizedDfa = dfa.minimize();
+
+                // Test the input strings
+                String[] testStrings = inString.getText().split(",");
+                outResult.clear(); // Clear previous results
+                StringBuilder testStringsResult = new StringBuilder();
+                int i = 0;
+                for (String testString : testStrings) {
+                    boolean isAccepted = data.isStringAccepted(testString);
+                    testStringsResult.append("String %d: %s have been %s\n".formatted(i + 1,testString ,isAccepted ? "Accepted" : "Rejected"));
+                    i++;
+                }
+
+                String getDotScript = minimizedDfa.generateDotScript();
+                String getOutput = "minimizedDfa.png";
+
+                try {
+                    minimizedDfa.GenerateImage(getDotScript, getOutput);
+                    Image image = new Image(new FileInputStream(getOutput));
+                    outGraph.setPreserveRatio(true);
+                    outGraph.setImage(image);
+                    GridPane.setHalignment(outGraph, HPos.CENTER); // Center horizontally in the cell
+                    GridPane.setValignment(outGraph, VPos.CENTER); // Center vertically in the cell
+                } catch (IOException | InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+
+                //display the output of minimized dfa and test strings result
+                outResult.setText(String.format("Minimized DFA:\n\nReachable state: %s\n\nEquivalent states: %s\n\nStates: %s\n\nAlphabet: %s\n\nStart State: %s\n\nFinal States: %s\n\nTransitions: %s\n\nString result:\n%s",
+                        dfa.getReachableStates(), dfa.findEquivalentStates(), minimizedDfa.state, minimizedDfa.alphabet, minimizedDfa.startState, minimizedDfa.finalState, minimizedDfa.transitions, testStringsResult));
+
+        });
     }
 
 }
